2a920de4bccb87705ed9573b259f95c09201b679
TPAIN22 <mamyafreka@gmail.com>
2025-12-09 12:09:15 +0300

    seo update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,60 @@
 export default function sitemap() {
+  const baseUrl = "https://nubian-sd.store";
+
   return [
     {
-      url: "https://nubian-sd.store",
-      lastModified: new Date(),
+      url: `${baseUrl}`,
+      lastModified: new Date().toISOString(),
       changeFrequency: "daily",
       priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/exchange-policy`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/terms-conditions`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/terms-of-use`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly",
+      priority: 0.6,
+    },
+    {
+      url: `${baseUrl}/sign-in`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+    {
+      url: `${baseUrl}/sign-up`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "yearly`",
+      priority: 0.3,
     },
   ];
 }
