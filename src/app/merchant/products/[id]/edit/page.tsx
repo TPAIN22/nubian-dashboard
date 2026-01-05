db8687550c37904fac14c52f611216650bee78b8
TPAIN22 <mamyafreka@gmail.com>
2026-01-06 01:57:18 +0300

    Refactor EditProductPage component: Update parameter handling to use promises for ID extraction, improving clarity and ensuring correct product ID is passed to the MerchantProductForm.

--- src/app/merchant/products/[id]/edit/page.tsx
+++ src/app/merchant/products/[id]/edit/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import { use } from 'react'
 import { MerchantProductForm } from '../../new/productForm'
 
-export default function EditProductPage({ params }: { params: { id: string } }) {
+export default function EditProductPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params)
   return (
     <div className="flex flex-col gap-4 h-full sm:mx-12 mx-2 py-4">
       <h1 className="text-2xl font-bold">تعديل المنتج</h1>
-      <MerchantProductForm productId={params.id} />
+      <MerchantProductForm productId={id} />
     </div>
   )
 }
